6cfac3ec873c317f409835fd7970b10adc905a1e
bbestbt <oranichbest@gmail.com>
2021-02-19 16:40:58 +0700

    add orn code

--- email-validation.js
+++ email-validation.js
@@ -1,31 +1,53 @@
-function ValidateEmail(inputText) {
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+// function ValidateEmail(inputText) {
+//     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-    if (inputText.match(mailformat)) {
-        //document.form1.text1.focus();
+//     if (inputText.match(mailformat)) {
+//         //document.form1.text1.focus();
 
-        return true;
-    } else {
-        alert("You have entered an invalid email address!");
-        //document.form1.text1.focus();
-        return false;
-    }
-}
-module.exports = ValidateEmail;
-
-
-// const button = document.getElementById('post-btn');
-
-// button.addEventListener('click', async _ => {
-//     try {
-//         const response = await fetch('file:///Users/orn/Desktop/DC/email.html', {
-//             method: 'post',
-//             body: {
-//               text: text1
-//             }
-//         });
-//         console.log('Completed!', response);
-//     } catch (err) {
-//         console.error(`Error: ${err}`);
+//         return true;
+//     } else {
+//         alert("You have entered an invalid email address!");
+//         //document.form1.text1.focus();
+//         return false;
 //     }
-// });
+// }
+// module.exports = ValidateEmail;
+
+function myFunction() {
+    var email;
+    email = document.getElementById("textEmail").value;
+    submitBtn.addEventListener('click',myFunction);
+    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+    if (reg.test(textEmail.value) == false) {
+        document.getElementById("demo").style.color = "red";
+        document.getElementById("demo").innerHTML = "Invalid EMail ->" + email;
+        alert('Invalid Email Address ->' + email);
+        return false;
+    } else {
+        document.getElementById("demo").style.color = "DarkGreen";
+        document.getElementById("demo").innerHTML = "Valid Email ->" + email;
+
+        $textEmail = $('#textEmail').val();
+        payload = {
+            email: $textEmail
+        }
+
+        $.ajax({
+            url: "http://0.0.0.0:8088/email",
+            //the data to send to
+            data: JSON.stringify(payload),
+            type: "POST",
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function(data) {
+                console.log('Completed!');
+            },
+            error: function() {
+                console.log('Error!');
+            }
+        });
+    }
+    return true;
+}
+ module.exports =myFunction ;
--- email.test.js
+++ email.test.js
@@ -1,5 +1,25 @@
-const ValidateEmail = require('./email-validation');
-var testMail="b@gmail.com"
-test('mail', () => {
-    expect(ValidateEmail(testMail)).toBe(true);
-  })
+// const ValidateEmail = require('./email-validation');
+// var testMail="b@gmail.com"
+// test('mail', () => {
+//     expect(ValidateEmail(testMail)).toBe(true);
+//   })
+
+
+test('Check email', () => {
+  document.body.innerHTML = `
+    <input id="textEmail" />
+    <input id="submitBtn"/>
+    <p id="demo" />
+  `;
+  require('./email-validation.js');
+
+  const textEmail = document.getElementById('textEmail');
+  const submitBtn = document.getElementById('submitBtn');
+  const demo=document.getElementById("demo");
+  textEmail.value = "n@mail.com";
+ // submitBtn.
+ expect(textEmail.value).toBe("n@mail.com");
+ // expect(demo.innerHTML).toBe("Valid Email ->n@mail.com");
+
+
+});
