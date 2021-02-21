35610b3c6cc743b0da754dacdda2edf290da6e3b
bbestbt <oranichbest@gmail.com>
2021-02-21 12:34:34 +0700

    delete comment

--- email.test.js
+++ email.test.js
@@ -5,21 +5,3 @@ test('mail', () => {
   })
 
 
-// test('Check email', () => {
-//   document.body.innerHTML = `
-//     <input id="textEmail" />
-//     <input id="btn"/>
-//     <p id="demo" />
-//   `;
-//   myFunction=require('./email-validation.js');
-//   const textEmail = document.getElementById('textEmail');
-//   const btn = document.getElementById('btn');
-//   const demo=document.getElementById("demo");
-
-//   textEmail.value = "n@mail.com";
-
-//  expect(textEmail.value).toBe("n@mail.com");
-//   // expect(demo.innerHTML).toBe("Valid Email ->n@mail.com");
-
-
-// });
